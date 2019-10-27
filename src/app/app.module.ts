40fd0b5d0b685034b0d7529b48b930960ac598af
Kamogelo <kamogeloCharmaine142@gmail.com>
2019-10-27 13:38:26 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,8 @@ import{RouterModule, Routes} from '@angular/router';
 
 const appRoutes: Routes =[
   {path :'products', component :ProductListComponent},
-  {path :'product/:id', component :ProductListComponent}
+  {path :'product/:id', component :ProductListComponent},
+  {path :'',redirectTo: 'products', pathMatch :'full'}
 ];
 
 
