24ee690912ed61555ce0c52a787f93de8f4932b8
Kamogelo <kamogeloCharmaine142@gmail.com>
2019-10-27 11:26:24 +0200

    Tracking List.

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class ProductService {
 
-    private _albumUrl = '../assets/album.json';
+  private _albumUrl = '../assets/album.json';
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album>
