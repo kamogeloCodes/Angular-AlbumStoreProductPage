634937d395303aa2e183c5f330ee1adce172e7cd
Kamogelo <kamogeloCharmaine142@gmail.com>
2019-10-27 12:49:39 +0200

    To be deleted.

---
+++ src/app/product-listng/product-listng.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-listng',
+  templateUrl: './product-listng.component.html',
+  styleUrls: ['./product-listng.component.css']
+})
+export class ProductListngComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
